b91ed296321749fb3384f2ed8f5482ba051a4be0
andre <andreroxhage74@gmail.com>
2024-04-02 21:50:53 +0200

    Vercel unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import Image from 'next/image';
 import Contact from './sections/Contact';
 import Header from './sections/Header';
 import About from './sections/About';
 import Photography from './sections/Photography';
 import CurrentWork from './sections/CurrentWork';
-import Resume from './components/Resume';
 import VoluntaryWork from './sections/VoluntaryWork';
-import {SparklesCore} from './components/sparkles';
 import ZoomParallax from './components/ZoomParallax/ZoomParallax';
 import Footer from './sections/Footer';
-import Test from './sections/Test';
 
 export default function Home() {
 	return (
