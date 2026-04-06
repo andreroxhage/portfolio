f813e1b826641a796db59fcbb16b25f0a6e1d52f
André Roxhage <andreroxhage74@gmail.com>
2026-04-06 14:53:21 +0200

    refactor: remove standalone About section from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Footer from '@/app/sections/Footer';
 import Header from '@/app/sections/Header';
-import About from '@/app/sections/About';
 import Photography from '@/app/sections/Photography';
 import CurrentWork from '@/app/sections/CurrentWork';
 import ZoomParallax from '@/app/components/ZoomParallax/ZoomParallax';
@@ -9,7 +8,6 @@ export default function Home() {
   return (
     <main className="h-full bg-secondary">
       <Header />
-      <About />
       <CurrentWork />
       <div className="bg-secondary">
         <Photography />
